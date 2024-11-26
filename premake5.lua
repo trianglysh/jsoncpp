d8e35bfb8e3aff33edb4867ccf1a474fc3fd8152
trianglysh <trianglxboi@gmail.com>
2024-11-26 20:44:51 +0300

    Project Files and Build Script

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "jsoncpp"
+	language "C++"
+	cppdialect "C++17"
+	kind "StaticLib"
+	
+	targetdir "%{OutputDir.Binaries}/%{prj.name}"
+	objdir "%{OutputDir.Intermediates}/%{prj.name}"
+	
+	files
+	{
+		"include/**.h",
+		"src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"include"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+	
+	filter "configurations:Development"
+		runtime "Debug"
+		symbols "On"
+		
+	filter "configurations:Preview or Shipping"
+		runtime "Debug"
+		symbols "On"
+	
+	
